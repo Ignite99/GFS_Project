1a453286bc9f024e9fb88ae195727db4a310467a
Goh Nicholas <nicholasgoh62@gmail.com>
2023-10-14 10:56:00 +0800

    main/chore: Added router base + Logs + templates

---
+++ helper/const.go
@@ -0,0 +1,8 @@
+package helper
+
+const (
+	BASE_URL = "http://localhost"
+
+	MASTER_SERVER_PORT = 8080
+	CLIENT_START_PORT  = 8081
+)
--- master/master.go
+++ master/master.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sutd_gfs_project/helper"
+)
+
+func RegisterRoutes(r *gin.Engine) {
+
+}
+
+func main() {
+	logfile, err := os.OpenFile("./logs/master_node.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatal("Error opening log file:", err)
+	}
+	defer logfile.Close()
+	log.SetOutput(logfile)
+
+	r := gin.Default()
+	RegisterRoutes(r)
+
+	r.Run(":" + strconv.Itoa(helper.MASTER_SERVER_PORT))
+}
