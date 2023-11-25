7466f7bf5a4712627504e273c62d78a981abecb2
lawraisins <97999866+lawraisins@users.noreply.github.com>
2023-11-25 21:27:45 +0800

    Create test folder and test script

---
+++ test/test.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+echo "Running Tests for GFS"
+echo "Starting Master Node"
+go run ../master/master.go
---
+++ test/tester.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+	"net/rpc"
+
+	"github.com/sutd_gfs_project/models"
+)
+
+// Testing get chunk location request
+func Test() {
+	client, err := rpc.Dial("tcp", "localhost:8080") // Replace with your master node's address
+	if err != nil {
+		log.Fatal("Error connecting to RPC server:", err)
+	}
+	defer client.Close()
+
+	args := models.ChunkLocationArgs{
+		Filename:   "file1.txt",
+		ChunkIndex: 1,
+	}
+	var reply models.ChunkMetadata
+
+	err = client.Call("MasterNode.GetChunkLocation", args, &reply)
+	if err != nil {
+		log.Fatal("Error calling RPC method: ", err)
+	}
+
+	log.Printf("Handle: %s, Location: %d\n", reply.Handle, reply.Location)
+}
