c34692a231ff1c8356cd61a07454c241b3e0bfd7
jaron <jaronho@protonmail.com>
2023-12-10 17:42:26 +0800

    fix: Split system test into two files to avoid errors

--- test/system_test.go
+++ test/system_test.go
@@ -4,14 +4,12 @@ import (
 	"flag"
 	"log"
 	"math/rand"
-	"net/rpc"
 	"os"
 	"strconv"
 	"testing"
 	"time"
 
 	"github.com/sutd_gfs_project/client"
-	"github.com/sutd_gfs_project/models"
 	"github.com/theritikchoure/logx"
 )
 
@@ -39,6 +37,8 @@ const (
 
 	CHARACTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789\n"
 )
+
+
 
 // Creates a byte array with random characters
 // Appends a sentence "Operation executed by Client X" at the end of the data
@@ -133,43 +133,6 @@ func run_write(c *client.Client) {
 		return
 	}
 	logx.Logf("[Client %d] Finished running...", logx.FGBLACK, logx.BGGREEN, c.ID)
-}
-func TestOperationsAfterKillingChunkServer(t *testing.T) {
-	logfile, _ := os.OpenFile("../logs/testing.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-	defer logfile.Close()
-	log.SetOutput(logfile)
-
-	log.Println("[System: OpsAfterKillingChunkServer] We will be killing one chunkserver for this test")
-	c, err := rpc.Dial("tcp", "localhost:8090") // Replace with your master node's address
-	if err != nil {
-		log.Fatal("[System: OpsAfterKillingChunkServer] Error connecting to RPC server:", err)
-	}
-	defer c.Close()
-
-	args := 1
-	var reply models.AckSigKill
-
-	err = c.Call("8090.Kill", args, &reply)
-	if err != nil {
-		log.Fatal("[System: OpsAfterKillingChunkServer] Error calling RPC method: ", err)
-	}
-
-	log.Printf("[System: OpsAfterKillingChunkServer] Killing for chunkserver at 8090 Ack: %v\n", reply.Ack)
-
-	log.Printf("[System: OpsAfterKillingChunkServer] Running Write Test after Killing Chunk Server..")
-	newClient1 := client.Client{ID: 0, OwnsLease: false, LeaseExpiryChan: make(chan bool, 1), RequestDone: make(chan bool, 1)}
-	data1 := []byte("This the write test.")
-	newClient1.CreateFile("testfile1.txt", data1)
-
-	log.Printf("[System: OpsAfterKillingChunkServer] Running Read Test after Killing Chunk Server....")
-	newClient2 := client.Client{ID: 99, OwnsLease: false, LeaseExpiryChan: make(chan bool, 1), RequestDone: make(chan bool, 1)}
-	newClient2.ReadFile("testfile1.txt", 0, 0)
-
-	log.Printf("[System: OpsAfterKillingChunkServer] Running Append Test after Killing Chunk Server....")
-	newClient3 := client.Client{ID: 99, OwnsLease: false, LeaseExpiryChan: make(chan bool, 1), RequestDone: make(chan bool, 1)}
-	data2 := []byte("This is the append test.")
-	newClient3.AppendToFile("testfile1.txt", data2)
-
 }
 
 // Test append with 10 clients
